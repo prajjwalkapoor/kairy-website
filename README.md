16126e24f0c905af60a0d264e5068145ed68d3a3
Prajjwal Kapoor <prajjwal.kapoor.mail@gmail.com>
2023-03-08 04:33:25 +0530

    landing page done

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kairy's Website
+
+Kairy is a browser extension to manage your bookmark shortcuts and reading lists.
---
+++ src/data.js
@@ -0,0 +1,32 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'View Github',
+      href: 'https://github.com/prajjwalkapoor/kairy',
+    },
+    {
+      text: 'Blog',
+      href: getBlogPermalink(),
+    },
+  ],
+  actions: [{ type: 'button', text: 'View Blog', href: getPermalink('/blog') }],
+};
+
+export const footerData = {
+  secondaryLinks: [
+    { text: 'View Github', href: 'https://github.com/prajjwalkapoor/kairy' },
+    { text: 'Blog', href: getPermalink('/blog') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/prajjwalkapoor/kairy' },
+  ],
+  footNote: `
+    Made by <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://prajwl.me/"> Prajjwal Kapoor</a> · All rights reserved.
+  `,
+};
